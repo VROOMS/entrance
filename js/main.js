724a0f273c3ef057f25488b73e8c48d6abfa8a38
jinredhub <jin@nieblo.com>
2018-01-09 00:18:21 -0800

    switched to github pages

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const mattgit = document.querySelector("#mattGithub");
+mattgit.addEventListener("click", function(){
+    window.open("https://github.com/metaversedance");
+})
+
+const jingit = document.querySelector("#jinGithub");
+jingit.addEventListener("click", function(){
+      window.open("https://github.com/jinredhub");
+})
