80ddff9502a001992f8fa44ff5e6e7ec4cb0e149
jinredhub <jin@nieblo.com>
2018-06-15 00:29:37 -0700

    added heroku wakeup

--- js/main.js
+++ js/main.js
@@ -7,3 +7,6 @@ const jingit = document.querySelector("#jinGithub");
 jingit.addEventListener("click", function(){
       window.open("https://github.com/jinredhub");
 })
+
+// wake up heroku
+$.ajax({url: "https://vrooms-editor.herokuapp.com/"});
