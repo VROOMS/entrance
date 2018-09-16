f301278810f2211c0ec6f90a0f9a8f2a3f9d458e
jinredhub <jin@nieblo.com>
2018-09-16 00:57:10 -0700

    added feature

--- js/main.js
+++ js/main.js
@@ -11,8 +11,3 @@ jingit.addEventListener("click", function(){
 // wake up heroku
 $.ajax({url: "https://vrooms-editor.herokuapp.com/"});
 
-// initiate wow.js
-new WOW({
-  mobile: false,
-  offset:  50
-}).init();
