fd8fbe9327bc01e8c5e9d67a26648ac2eca3d9f8
jinredhub <jin@nieblo.com>
2018-09-17 22:11:58 -0700

    added step belt

--- js/main.js
+++ js/main.js
@@ -11,3 +11,23 @@ jingit.addEventListener("click", function(){
 // wake up heroku
 $.ajax({url: "https://vrooms-editor.herokuapp.com/"});
 
+// add active class to current button and move belt
+const stepBtnContainer = document.querySelector('.instruction__stepButtonContainer');
+const stepBtns = document.querySelectorAll('.instruction__stepButtonContainer__item');
+const instructionBelt = document.querySelector('.instruction__belt');
+
+for(let i=0; i< stepBtns.length; i++){
+	stepBtns[i].onclick = function(){
+		const current = document.querySelector('.active');
+		current.className = current.className.replace(' active', '');
+		this.className += ' active';
+
+		if(this.getAttribute('data-LR') === 'right'){
+			instructionBelt.style.left = '-100%';	
+		}
+		else{
+			instructionBelt.style.left = '0';
+		}
+		
+	}
+}
