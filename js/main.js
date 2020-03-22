ebb08ee51ba97e28bd438b91a1f6c27bee8c579e
jinredhub <jin@nieblo.com>
2020-03-21 23:26:16 -0700

    update step css

--- js/main.js
+++ js/main.js
@@ -1,12 +1,12 @@
-const mattgit = document.querySelector("#mattGithub");
-mattgit.addEventListener("click", function(){
-    window.open("https://github.com/metaversedance");
-})
+// const mattgit = document.querySelector("#mattGithub");
+// mattgit.addEventListener("click", function(){
+//     window.open("https://github.com/metaversedance");
+// })
 
-const jingit = document.querySelector("#jinGithub");
-jingit.addEventListener("click", function(){
-      window.open("https://github.com/jinredhub");
-})
+// const jingit = document.querySelector("#jinGithub");
+// jingit.addEventListener("click", function(){
+//       window.open("https://github.com/jinredhub");
+// })
 
 // wake up heroku
 $.ajax({url: "https://vrooms-editor.herokuapp.com/"});
